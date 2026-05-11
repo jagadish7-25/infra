2b1149e09e0f344edeb7382cd75dcb306dc3204e
jagadish7-25 <kadapajag9@gmail.com>
2026-05-11 13:39:27 +0530

    modifed

--- main.tf
+++ main.tf
@@ -8,9 +8,7 @@ data "aws_subnets" "selected" {
 resource "aws_instance" "Docker" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = data.aws_subnets.selected.ids[0]
   vpc_security_group_ids = [var.security_group_id]
-
   associate_public_ip_address = true
 
   tags = {
